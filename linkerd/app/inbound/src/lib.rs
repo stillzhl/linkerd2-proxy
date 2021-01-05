53a7881a8e5f1c818863da7652edab51c5c504c7
Oliver Gould <ver@buoyant.io>
2021-01-05 06:00:08 +0000

    Split stack builders into module-level functions

--- linkerd/app/inbound/src/lib.rs
+++ linkerd/app/inbound/src/lib.rs
@@ -70,8 +70,8 @@ pub fn tcp_connect<T: Into<u16>>(
         .into_inner()
 }
 
-#[allow(clippy::too_many_arguments)]
 impl Config {
+    #[allow(clippy::too_many_arguments)]
     pub fn build<I, P, C, H, HSvc, T, TSvc>(
         self,
         listen_addr: SocketAddr,
@@ -79,10 +79,10 @@ impl Config {
         connect: C,
         http_gateway: H,
         tcp_gateway: Option<T>,
-        profiles_client: P,
+        profiles: P,
         tap: tap::Registry,
         metrics: metrics::Proxy,
-        span_sink: Option<mpsc::Sender<oc::Span>>,
+        traces: Option<mpsc::Sender<oc::Span>>,
         drain: drain::Watch,
     ) -> impl svc::NewService<
         listen::Addrs,
@@ -171,15 +171,21 @@ impl Config {
                     // If there's no opaque transport header, try to detect
                     // HTTP. If that can't be done, fail the connection as if it
                     // were refused.
-                    svc::stack(self.http_server(http, &metrics, span_sink.clone(), drain.clone()))
-                        .push(svc::NewUnwrapOr::layer(
-                            svc::Fail::<_, NonOpaqueRefused>::default(),
-                        ))
-                        .push(NewDetectService::layer(
-                            self.proxy.detect_protocol_timeout,
-                            http::DetectHttp::default(),
-                        ))
-                        .into_inner(),
+                    svc::stack(http_server(
+                        &self.proxy,
+                        http,
+                        &metrics,
+                        traces.clone(),
+                        drain.clone(),
+                    ))
+                    .push(svc::NewUnwrapOr::layer(
+                        svc::Fail::<_, NonOpaqueRefused>::default(),
+                    ))
+                    .push(NewDetectService::layer(
+                        self.proxy.detect_protocol_timeout,
+                        http::DetectHttp::default(),
+                    ))
+                    .into_inner(),
                 ))
                 .push(NewDetectService::layer(
                     self.proxy.detect_protocol_timeout,
@@ -189,9 +195,8 @@ impl Config {
         };
 
         let http = {
-            let router =
-                self.http_router(connect, profiles_client, tap, &metrics, span_sink.clone());
-            self.http_server(router, &metrics, span_sink, drain)
+            let router = http_router(&self, connect, profiles, tap, &metrics, traces.clone());
+            http_server(&self.proxy, router, &metrics, traces, drain)
         };
 
         svc::stack(http)
@@ -227,182 +232,177 @@ impl Config {
             )
             .into_inner()
     }
+}
 
-    fn http_router<T, C, P>(
-        &self,
-        connect: C,
-        profiles_client: P,
-        tap: tap::Registry,
-        metrics: &metrics::Proxy,
-        span_sink: Option<mpsc::Sender<oc::Span>>,
-    ) -> impl svc::NewService<
-        T,
-        Service = impl svc::Service<
-            http::Request<http::BoxBody>,
-            Response = http::Response<http::BoxBody>,
-            Error = Error,
-            Future = impl Send,
-        > + Clone,
-    > + Clone
-    where
-        RequestTarget: From<T>,
-        C: svc::Service<TcpEndpoint> + Clone + Send + Sync + Unpin + 'static,
-        C::Response: io::AsyncRead + io::AsyncWrite + Send + Unpin + 'static,
-        C::Error: Into<Error>,
-        C::Future: Send + Unpin,
-        P: profiles::GetProfile<NameAddr> + Clone + Send + Sync + 'static,
-        P::Future: Send,
-        P::Error: Send,
-    {
-        // Creates HTTP clients for each inbound port & HTTP settings.
-        let endpoint = svc::stack(connect)
-            .push(metrics.transport.layer_connect())
-            .push_map_target(TcpEndpoint::from)
-            .push(http::client::layer(
-                self.proxy.connect.h1_settings,
-                self.proxy.connect.h2_settings,
-            ))
-            .push(reconnect::layer({
-                let backoff = self.proxy.connect.backoff;
-                move |_| Ok(backoff.stream())
-            }))
-            .check_new_service::<HttpEndpoint, http::Request<_>>();
+fn http_router<T, C, P>(
+    config: &Config,
+    connect: C,
+    profiles: P,
+    tap: tap::Registry,
+    metrics: &metrics::Proxy,
+    traces: Option<mpsc::Sender<oc::Span>>,
+) -> impl svc::NewService<
+    T,
+    Service = impl svc::Service<
+        http::Request<http::BoxBody>,
+        Response = http::Response<http::BoxBody>,
+        Error = Error,
+        Future = impl Send,
+    > + Clone,
+> + Clone
+where
+    RequestTarget: From<T>,
+    C: svc::Service<TcpEndpoint> + Clone + Send + Sync + Unpin + 'static,
+    C::Response: io::AsyncRead + io::AsyncWrite + Send + Unpin + 'static,
+    C::Error: Into<Error>,
+    C::Future: Send + Unpin,
+    P: profiles::GetProfile<NameAddr> + Clone + Send + Sync + 'static,
+    P::Future: Send,
+    P::Error: Send,
+{
+    // Creates HTTP clients for each inbound port & HTTP settings.
+    let endpoint = svc::stack(connect)
+        .push(metrics.transport.layer_connect())
+        .push_map_target(TcpEndpoint::from)
+        .push(http::client::layer(
+            config.proxy.connect.h1_settings,
+            config.proxy.connect.h2_settings,
+        ))
+        .push(reconnect::layer({
+            let backoff = config.proxy.connect.backoff;
+            move |_| Ok(backoff.stream())
+        }))
+        .check_new_service::<HttpEndpoint, http::Request<_>>();
 
-        let target = endpoint
-            .push_map_target(HttpEndpoint::from)
-            // Registers the stack to be tapped.
-            .push(tap::NewTapHttp::layer(tap))
-            // Records metrics for each `Target`.
-            .push(metrics.http_endpoint.to_layer::<classify::Response, _>())
-            .push_on_response(TraceContext::layer(
-                span_sink.map(|span_sink| SpanConverter::client(span_sink, trace_labels())),
-            ))
-            .push_on_response(http::BoxResponse::layer())
-            .check_new_service::<Target, http::Request<_>>();
+    let target = endpoint
+        .push_map_target(HttpEndpoint::from)
+        // Registers the stack to be tapped.
+        .push(tap::NewTapHttp::layer(tap))
+        // Records metrics for each `Target`.
+        .push(metrics.http_endpoint.to_layer::<classify::Response, _>())
+        .push_on_response(TraceContext::layer(
+            traces.map(|traces| SpanConverter::client(traces, trace_labels())),
+        ))
+        .push_on_response(http::BoxResponse::layer())
+        .check_new_service::<Target, http::Request<_>>();
 
-        // Attempts to discover a service profile for each logical target (as
-        // informed by the request's headers). The stack is cached until a
-        // request has not been received for `cache_max_idle_age`.
-        let profile = target
-            .clone()
-            .check_new_service::<Target, http::Request<http::BoxBody>>()
-            .push_on_response(http::BoxRequest::layer())
-            // The target stack doesn't use the profile resolution, so drop it.
-            .push_map_target(endpoint::Target::from)
-            .push(profiles::http::route_request::layer(
-                svc::proxies()
-                    // Sets the route as a request extension so that it can be used
-                    // by tap.
-                    .push_http_insert_target()
-                    // Records per-route metrics.
-                    .push(metrics.http_route.to_layer::<classify::Response, _>())
-                    // Sets the per-route response classifier as a request
-                    // extension.
-                    .push(classify::NewClassify::layer())
-                    .check_new_clone::<dst::Route>()
-                    .push_map_target(endpoint::route)
-                    .into_inner(),
-            ))
-            .push_map_target(endpoint::Logical::from)
-            .push(profiles::discover::layer(
-                profiles_client,
-                AllowProfile(self.allow_discovery.clone()),
-            ))
-            .push_on_response(http::BoxResponse::layer())
-            .instrument(|_: &Target| debug_span!("profile"))
-            // Skip the profile stack if it takes too long to become ready.
-            .push_when_unready(target.clone(), self.profile_idle_timeout)
-            .check_new_service::<Target, http::Request<http::BoxBody>>();
+    // Attempts to discover a service profile for each logical target (as
+    // informed by the request's headers). The stack is cached until a
+    // request has not been received for `cache_max_idle_age`.
+    let profile = target
+        .clone()
+        .check_new_service::<Target, http::Request<http::BoxBody>>()
+        .push_on_response(http::BoxRequest::layer())
+        // The target stack doesn't use the profile resolution, so drop it.
+        .push_map_target(endpoint::Target::from)
+        .push(profiles::http::route_request::layer(
+            svc::proxies()
+                // Sets the route as a request extension so that it can be used
+                // by tap.
+                .push_http_insert_target()
+                // Records per-route metrics.
+                .push(metrics.http_route.to_layer::<classify::Response, _>())
+                // Sets the per-route response classifier as a request
+                // extension.
+                .push(classify::NewClassify::layer())
+                .check_new_clone::<dst::Route>()
+                .push_map_target(endpoint::route)
+                .into_inner(),
+        ))
+        .push_map_target(endpoint::Logical::from)
+        .push(profiles::discover::layer(
+            profiles,
+            AllowProfile(config.allow_discovery.clone()),
+        ))
+        .push_on_response(http::BoxResponse::layer())
+        .instrument(|_: &Target| debug_span!("profile"))
+        // Skip the profile stack if it takes too long to become ready.
+        .push_when_unready(target.clone(), config.profile_idle_timeout)
+        .check_new_service::<Target, http::Request<http::BoxBody>>();
 
-        // If the traffic is targeted at the inbound port, send it through
-        // the loopback service (i.e. as a gateway).
-        svc::stack(profile)
-            .check_new_service::<Target, http::Request<http::BoxBody>>()
-            .push_on_response(
-                svc::layers()
-                    .push(svc::FailFast::layer("Logical", self.proxy.dispatch_timeout))
-                    .push_spawn_buffer(self.proxy.buffer_capacity)
-                    .push(metrics.stack.layer(stack_labels("http", "logical"))),
-            )
-            .push_cache(self.proxy.cache_max_idle_age)
-            .push_on_response(
-                svc::layers()
-                    .push(http::Retain::layer())
-                    .push(http::BoxResponse::layer()),
-            )
-            // Boxing is necessary purely to limit the link-time overhead of
-            // having enormous types.
-            .push(svc::BoxNewService::layer())
-            .check_new_service::<Target, http::Request<http::BoxBody>>()
-            // Removes the override header after it has been used to
-            // determine a reuquest target.
-            .push_on_response(strip_header::request::layer(DST_OVERRIDE_HEADER))
-            // Routes each request to a target, obtains a service for that
-            // target, and dispatches the request.
-            .instrument_from_target()
-            .push(svc::NewRouter::layer(RequestTarget::from))
-            .into_inner()
-    }
+    // If the traffic is targeted at the inbound port, send it through
+    // the loopback service (i.e. as a gateway).
+    svc::stack(profile)
+        .check_new_service::<Target, http::Request<http::BoxBody>>()
+        .push_on_response(
+            svc::layers()
+                .push(svc::FailFast::layer(
+                    "Logical",
+                    config.proxy.dispatch_timeout,
+                ))
+                .push_spawn_buffer(config.proxy.buffer_capacity)
+                .push(metrics.stack.layer(stack_labels("http", "logical"))),
+        )
+        .push_cache(config.proxy.cache_max_idle_age)
+        .push_on_response(
+            svc::layers()
+                .push(http::Retain::layer())
+                .push(http::BoxResponse::layer()),
+        )
+        // Boxing is necessary purely to limit the link-time overhead of
+        // having enormous types.
+        .push(svc::BoxNewService::layer())
+        .check_new_service::<Target, http::Request<http::BoxBody>>()
+        // Removes the override header after it has been used to
+        // determine a reuquest target.
+        .push_on_response(strip_header::request::layer(DST_OVERRIDE_HEADER))
+        // Routes each request to a target, obtains a service for that
+        // target, and dispatches the request.
+        .instrument_from_target()
+        .push(svc::NewRouter::layer(RequestTarget::from))
+        .into_inner()
+}
 
-    fn http_server<T, I, H, HSvc>(
-        &self,
-        http: H,
-        metrics: &metrics::Proxy,
-        span_sink: Option<mpsc::Sender<oc::Span>>,
-        drain: drain::Watch,
-    ) -> impl svc::NewService<
-        (http::Version, T),
-        Service = impl svc::Service<I, Response = (), Error = Error, Future = impl Send> + Clone,
-    > + Clone
-    where
-        T: Clone + Send + Sync + Unpin + 'static,
-        for<'t> &'t T: Into<SocketAddr>,
-        I: io::AsyncRead + io::AsyncWrite + io::PeerAddr + Send + Unpin + 'static,
-        H: svc::NewService<T, Service = HSvc> + Clone + Send + 'static,
-        HSvc: svc::Service<http::Request<http::BoxBody>, Response = http::Response<http::BoxBody>>
-            + Clone
-            + Send
-            + Unpin
-            + 'static,
-        HSvc::Error: Into<Error>,
-        HSvc::Future: Send,
-    {
-        svc::stack(http)
-            .push_http_insert_target() // Used by tap.
-            .push_on_response(
-                svc::layers()
-                    // Downgrades the protocol if upgraded by an outbound proxy.
-                    .push(orig_proto::Downgrade::layer())
-                    // Limits the number of in-flight requests.
-                    .push(svc::ConcurrencyLimit::layer(
-                        self.proxy.max_in_flight_requests,
-                    ))
-                    // Eagerly fail requests when the proxy is out of capacity for a
-                    // dispatch_timeout.
-                    .push(svc::FailFast::layer(
-                        "HTTP Server",
-                        self.proxy.dispatch_timeout,
-                    ))
-                    .push(metrics.http_errors.clone())
-                    // Synthesizes responses for proxy errors.
-                    .push(errors::layer())
-                    .push(TraceContext::layer(span_sink.map(|span_sink| {
-                        SpanConverter::server(span_sink, trace_labels())
-                    })))
-                    .push(metrics.stack.layer(stack_labels("http", "server")))
-                    .push(http::BoxResponse::layer())
-                    .push(http::BoxRequest::layer()),
-            )
-            .push(http::NewNormalizeUri::layer())
-            .push_map_target(|(_, t): (_, T)| t)
-            .instrument(|(v, _): &(http::Version, _)| debug_span!("http", %v))
-            .push(http::NewServeHttp::layer(
-                self.proxy.server.h2_settings,
-                drain,
-            ))
-            .check_new_service::<(http::Version, T), I>()
-            .into_inner()
-    }
+fn http_server<T, I, H, HSvc>(
+    config: &ProxyConfig,
+    http: H,
+    metrics: &metrics::Proxy,
+    traces: Option<mpsc::Sender<oc::Span>>,
+    drain: drain::Watch,
+) -> impl svc::NewService<
+    (http::Version, T),
+    Service = impl svc::Service<I, Response = (), Error = Error, Future = impl Send> + Clone,
+> + Clone
+where
+    T: Clone + Send + Sync + Unpin + 'static,
+    for<'t> &'t T: Into<SocketAddr>,
+    I: io::AsyncRead + io::AsyncWrite + io::PeerAddr + Send + Unpin + 'static,
+    H: svc::NewService<T, Service = HSvc> + Clone + Send + 'static,
+    HSvc: svc::Service<http::Request<http::BoxBody>, Response = http::Response<http::BoxBody>>
+        + Clone
+        + Send
+        + Unpin
+        + 'static,
+    HSvc::Error: Into<Error>,
+    HSvc::Future: Send,
+{
+    svc::stack(http)
+        .push_http_insert_target() // Used by tap.
+        .push_on_response(
+            svc::layers()
+                // Downgrades the protocol if upgraded by an outbound proxy.
+                .push(orig_proto::Downgrade::layer())
+                // Limits the number of in-flight requests.
+                .push(svc::ConcurrencyLimit::layer(config.max_in_flight_requests))
+                // Eagerly fail requests when the proxy is out of capacity for a
+                // dispatch_timeout.
+                .push(svc::FailFast::layer("HTTP Server", config.dispatch_timeout))
+                .push(metrics.http_errors.clone())
+                // Synthesizes responses for proxy errors.
+                .push(errors::layer())
+                .push(TraceContext::layer(
+                    traces.map(|traces| SpanConverter::server(traces, trace_labels())),
+                ))
+                .push(metrics.stack.layer(stack_labels("http", "server")))
+                .push(http::BoxResponse::layer())
+                .push(http::BoxRequest::layer()),
+        )
+        .push(http::NewNormalizeUri::layer())
+        .push_map_target(|(_, t): (_, T)| t)
+        .instrument(|(v, _): &(http::Version, _)| debug_span!("http", %v))
+        .push(http::NewServeHttp::layer(config.server.h2_settings, drain))
+        .check_new_service::<(http::Version, T), I>()
+        .into_inner()
 }
 
 pub fn trace_labels() -> HashMap<String, String> {
